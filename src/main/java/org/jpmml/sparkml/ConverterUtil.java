d18777c2cf6e8a52879ec42b537fd6de34be241d
Shuyi Wang <sdjksdafji@gmail.com>
2016-11-08 11:17:03 -0800

    add bytes conversion

--- src/main/java/org/jpmml/sparkml/ConverterUtil.java
+++ src/main/java/org/jpmml/sparkml/ConverterUtil.java
@@ -18,14 +18,8 @@
  */
 package org.jpmml.sparkml;
 
-import java.lang.reflect.Constructor;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
 import com.google.common.collect.Iterables;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.spark.ml.Model;
 import org.apache.spark.ml.PipelineModel;
 import org.apache.spark.ml.Transformer;
@@ -70,6 +64,7 @@ import org.dmg.pmml.PMML;
 import org.jpmml.converter.MiningModelUtil;
 import org.jpmml.converter.ModelUtil;
 import org.jpmml.converter.Schema;
+import org.jpmml.model.MetroJAXBUtil;
 import org.jpmml.sparkml.feature.BinarizerConverter;
 import org.jpmml.sparkml.feature.BucketizerConverter;
 import org.jpmml.sparkml.feature.ChiSqSelectorModelConverter;
@@ -95,6 +90,14 @@ import org.jpmml.sparkml.model.LogisticRegressionModelConverter;
 import org.jpmml.sparkml.model.MultilayerPerceptronClassificationModelConverter;
 import org.jpmml.sparkml.model.RandomForestClassificationModelConverter;
 import org.jpmml.sparkml.model.RandomForestRegressionModelConverter;
+
+import javax.xml.bind.JAXBException;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ConverterUtil {
 
@@ -214,6 +217,21 @@ public class ConverterUtil {
 		PMML pmml = featureMapper.encodePMML(rootModel);
 
 		return pmml;
+	}
+
+	static
+	public byte[] toPMMLByteArray(StructType schema, PipelineModel pipelineModel){
+		PMML pmml = toPMML(schema, pipelineModel);
+
+		ByteArrayOutputStream os = new ByteArrayOutputStream(1024 * 1024);
+
+		try {
+			MetroJAXBUtil.marshalPMML(pmml, os);
+		} catch(JAXBException je){
+			throw new RuntimeException(je);
+		}
+
+		return os.toByteArray();
 	}
 
 	static
